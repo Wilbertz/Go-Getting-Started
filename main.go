93c17b1649df56ffa08e666f52ee541a06516c55
Harald Wilbertz <harald.wilbertz@gmx.de>
2022-07-12 16:47:19 +0200

    Address operations added.

--- main.go
+++ main.go
@@ -3,12 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	firstName := "Harald"
-	fmt.Println(firstName)
-
-	ptr := &firstName
-	fmt.Println(ptr, *ptr)
-
-	firstName = "Tanja"
-	fmt.Println(ptr, *ptr)
+	const pi = 3.14159
+	fmt.Println(pi)
 }
