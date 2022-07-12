960f2a85cc8f0f52baca3593c1da55db3ecb9c43
Harald Wilbertz <harald.wilbertz@gmx.de>
2022-07-12 16:13:28 +0200

    Pointers added.

--- main.go
+++ main.go
@@ -3,22 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var i int
-	i = 42
-	fmt.Println(i)
-
-	var f float32 = 3.14
-	fmt.Println(f)
-
-	firstName := "Harald"
-	fmt.Println(firstName)
-
-	b := true
-	fmt.Println(b)
-
-	c := complex(3, 4)
-	fmt.Println(c)
-
-	r, im := real(c), imag(c)
-	fmt.Println(r, im)
+	var firstName *string = new(string)
+	*firstName = "Harald"
+	fmt.Print(*firstName)
 }
