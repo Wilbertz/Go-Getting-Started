8d453664bc60bbda9d8c7e20c872d2270aea87b9
Harald Wilbertz <harald.wilbertz@gmx.de>
2022-07-13 12:23:05 +0200

    Packages models added.

--- main.go
+++ main.go
@@ -1,20 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-	type user struct {
-		ID        int
-		FirstName string
-		LastName  string
-	}
-	var u user
-	u.ID = 1
-	u.FirstName = "Harald"
-	u.LastName = "Wilbertz"
-	fmt.Println(u)
-	fmt.Println(u.FirstName)
 
-	u2 := user{ID: 1, FirstName: "Arthur", LastName: "Dent"}
-	fmt.Println(u2)
 }
