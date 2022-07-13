1125b882385342ee1288454d2c9803a27c597631
Harald Wilbertz <harald.wilbertz@gmx.de>
2022-07-13 13:46:00 +0200

    function added.

--- main.go
+++ main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
-	"Go-Getting-Started/models"
 	"fmt"
 )
 
 func main() {
-	u := models.User{
-		ID:        2,
-		FirstName: "Tricia",
-		LastName:  "McMillan",
-	}
+	//u := models.User{
+	//	ID:        2,
+	//	FirstName: "Tricia",
+	//	LastName:  "McMillan",
+	//}
+	//fmt.Println("Hello Playground")
 
-	fmt.Println(u)
+	port := 3000
+	startWebServer(port, 2)
+}
+
+func startWebServer(port int, numberOfRetries int) {
+	fmt.Println("Starting server...")
+	// Do something
+	fmt.Println("Server started on port", port)
+	fmt.Println("Number of retries", numberOfRetries)
 }
