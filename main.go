5b7bedf3fa0bad9f1b032c7423c1c18ceca40eee
Harald Wilbertz <harald.wilbertz@gmx.de>
2022-07-13 12:54:16 +0200

    Packages models added.

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"Go-Getting-Started/models"
+	"fmt"
+)
 
+func main() {
+	u := models.User{
+		ID:        2,
+		FirstName: "Tricia",
+		LastName:  "McMillan",
+	}
+
+	fmt.Println(u)
 }
