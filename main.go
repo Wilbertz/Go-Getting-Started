e517de59237944f4c511e2157c3fd864d989b7a0
Harald Wilbertz <harald.wilbertz@gmx.de>
2022-07-13 11:24:38 +0200

    Slice resized.

--- main.go
+++ main.go
@@ -3,12 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	arr := [3]int{1, 2, 3}
-	slice := arr[:]
-
-	arr[1] = 42
-	slice[2] = 27
-
-	fmt.Println(arr)
+	slice := []int{1, 2, 3}
 	fmt.Println(slice)
+
+	slice = append(slice, 4, 42, 27)
+	fmt.Println(slice)
+	slice2 := slice[1:]
+	fmt.Println(slice2)
 }
