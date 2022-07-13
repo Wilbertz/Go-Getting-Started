4849a3bee78b55496bad14e2ae893175083a2f12
Harald Wilbertz <harald.wilbertz@gmx.de>
2022-07-13 10:41:30 +0200

    Array added.

--- main.go
+++ main.go
@@ -2,12 +2,10 @@ package main
 
 import "fmt"
 
-const (
-	first = iota
-	second
-	third
-)
-
 func main() {
-	fmt.Println(first, second, third)
+	var arr [3]int
+	arr[0] = 1
+	arr[1] = 2
+	arr[2] = 3
+	fmt.Println(arr)
 }
