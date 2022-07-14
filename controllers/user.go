b8dc751ccff5cd672ed70a0da19c5cd444a69013
Harald Wilbertz <harald.wilbertz@gmx.de>
2022-07-14 11:39:48 +0200

    Constructor method added.

--- controllers/user.go
+++ controllers/user.go
@@ -1,3 +1,20 @@
 package controllers
 
-type userController struct{}
+import (
+	"net/http"
+	"regexp"
+)
+
+type userController struct {
+	userIDPattern *regexp.Regexp
+}
+
+func (uc userController) ServeHttp(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from the User Controller!"))
+}
+
+func newUserController() *userController {
+	return &userController{
+		userIDPattern: regexp.MustCompile(`^/users/(\d+)`),
+	}
+}
