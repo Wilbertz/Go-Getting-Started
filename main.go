38db7b1bb1fd1899d1ca2da539835695314d62e8
Harald Wilbertz <harald.wilbertz@gmx.de>
2022-07-14 12:36:50 +0200

    Controller finished.

--- main.go
+++ main.go
@@ -1,27 +1,11 @@
 package main
 
 import (
-	"fmt"
+	"Go-Getting-Started/controllers"
+	"net/http"
 )
 
 func main() {
-	//u := models.User{
-	//	ID:        2,
-	//	FirstName: "Tricia",
-	//	LastName:  "McMillan",
-	//}
-	//fmt.Println("Hello Playground")
-
-	port := 3000
-	_, err := startWebServer(port, 2)
-	fmt.Println(err)
-}
-
-func startWebServer(port int, numberOfRetries int) (int, error) {
-	fmt.Println("Starting server...")
-	// Do something
-	fmt.Println("Server started on port", port)
-	fmt.Println("Number of retries", numberOfRetries)
-
-	return port, nil
+	controllers.RegisterControllers()
+	http.ListenAndServe(":3000", nil)
 }
